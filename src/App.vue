f9297ca71e7aa9b70dac3438604c8f44b2befccd
wanwan <tel18810162535@163.com>
2019-07-25 17:19:03 +0800

    第一次，已经整合到了header部分的功能已完成

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <VHeader :seller="seller"></VHeader>
+  </div>
+</template>
+
+<script>
+  import VHeader from 'components/v-header/v-header'
+  import { getSeller } from 'api'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        seller: {
+        }
+      }
+    },
+    created () {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller().then((seller) => {
+          this.seller = seller
+        })
+      }
+    },
+    components: {
+      VHeader
+    }
+  }
+</script>
+<style lang="stylus">
+</style>
